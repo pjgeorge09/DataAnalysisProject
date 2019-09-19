38f5f28c3ecd64b8dc7efd155d16b58ff74abfbb
Pete George <georgepj@vcu.edu>
2019-09-19 11:47:53 -0400

    imputed mean & conditional are working for 5% and 20%

--- a2.py
+++ a2.py
@@ -9,6 +9,11 @@ Created on Thu Sep 12 12:06:51 2019
 
 import pandas as pd
 
+def printHT(toPrint):
+    print(toPrint.head(8))
+    print(toPrint.tail(8))
+    return
+
 '''A method to get the average value of any column, ignoring question mark values'''
 def getColAve(object1):
     counter = 0
@@ -30,59 +35,53 @@ def replaceWithAverage(theDF):
     for i in aDF:
         if (i != 'Class'):
             ave = getColAve(aDF[i])
-            aDF[i] = aDF[i].replace('?',str(ave))            
+            aDF[i] = aDF[i].replace('?', value = str(ave), inplace=False)            
     return aDF
 
+    
+'''Replace with average of matching data for similarity'''
+def replaceWithConditionalAverage(theDF):
+    #Split the data files into two DF objects with Yes and No respectively
+    yesDF  = theDF[theDF.Class == 'Yes']
+    noDF  = theDF[theDF.Class == 'No']
+    #Replace the missing values with the average of their isolated data columns
+    yesDF = replaceWithAverage(yesDF)
+    noDF = replaceWithAverage(noDF)
+    #https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html
+    #Join the two dataframe objects back together
+    frames = [yesDF, noDF]
+    toReturn = pd.concat(frames)
+    return toReturn
 
 '''Read the files into python'''
 df = pd.read_csv('dataset_complete.csv')
 df05 = pd.read_csv('dataset_missing05.csv')
 df20 = pd.read_csv('dataset_missing20.csv')
 dfmock05 = pd.read_csv('dataset_missing05.csv')
-#print(df)
 
-'''Column means calculated here'''
 
+'''dataset with 5% missing computed with mean here''' 
+'''TODO: Calculate on the 20% missing data '''
+#allCol is new list object of the dfmock05 dataset
 allCol = list(dfmock05)
-dfmock05 = replaceWithAverage(dfmock05)
-print(dfmock05)
-
-col_05_F1 = list(df05.F1)
-aF1 = getColAve(col_05_F1)
-
-
-col_05_F2 = list(df05.F2)
-aF2 = getColAve(col_05_F2)
-
-col_05_F3 = list(df05.F3)
-aF3 = getColAve(col_05_F3)
-
-col_05_F4 = list(df05.F4)
-aF4 = getColAve(col_05_F4)
-
-col_05_F5 = list(df05.F5)
-aF5 = getColAve(col_05_F5)
-
-col_05_F6 = list(df05.F6)
-aF6 = getColAve(col_05_F6)
-
-col_05_F7 = list(df05.F7)
-aF7 = getColAve(col_05_F7)
-
-col_05_F8 = list(df05.F8)
-aF8 = getColAve(col_05_F8)  
-
-'''propogate mock dataframe, fill in ?'''
+##printHT(dfmock05) #Print for pre-test
+#Parse each column, calculate the average in that column, replace all '?', iterate
+dfmock05 = replaceWithAverage(dfmock05) 
+##printHT(dfmock05) #Print for post-test
+#use export to set this temporary dfmock05 dataset, adjusted, to filename below
+export = dfmock05.to_csv('V00396834_missing05_imputed_mean.csv', index= None, header=True)
 
 
-'''Testing a write to function'''
-
-
-        
-
-#print(col05F1)   
-#df05['F1'] = df05['F1'].astype(float)
-
+print("\n------------Conditional Mean Imputation Start-------------\n")
+'''Setting up for algorithm 2 Conditional Mean Imputation on 5% missing'''
+'''TODO: Calculate on the 20% missing data '''
+#Reset dfmock05 to the origial dataset from 'dataset_missing05.csv'
+dfmock05 = pd.read_csv('dataset_missing05.csv')
+allCol = list(dfmock05) #set allCol to list form of dfmock05
+##printHT(dfmock05) #Print for pre-test
+dfmock05 = replaceWithConditionalAverage(dfmock05)
+##printHT(dfmock05) #Print for post-test
+export = dfmock05.to_csv('V00396834_missing05_imputed_mean_conditional.csv', index=None, header=True)
 
 
 

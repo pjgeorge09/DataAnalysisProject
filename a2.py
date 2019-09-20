93701a7ba978d5c07a4438cefcc673ee1449103c
Pete George <georgepj@vcu.edu>
2019-09-19 21:55:47 -0400

    Working on getting the Euclidean Distance to work

--- a2.py
+++ a2.py
@@ -8,6 +8,8 @@ Created on Thu Sep 12 12:06:51 2019
 """
 
 import pandas as pd
+import math
+import time
 
 def printHT(toPrint):
     print(toPrint.head(8))
@@ -18,9 +20,9 @@ def printHT(toPrint):
 def getColAve(object1):
     counter = 0
     runningSum = 0
-    # For Each Column (i) in the Dataframe (object1) do stuff
+    # For Each Row Item (i) in the passed column
     for i in object1:
-        # For Each Value in Column Above that does not equal '?'   do stuff
+        # that is not equal to '?'
         if (i != '?'):
             # Make the value a float
             i = float(i)
@@ -37,7 +39,9 @@ def getColAve(object1):
 def replaceWithAverage(theDF):
     aDF = theDF
     ave = 0
+    # For Each Column in the Dataframe Object    do stuff
     for i in aDF:
+        # For Each Column that is not 'Class'    do stuff
         if (i != 'Class'):
             ave = getColAve(aDF[i])
             aDF[i] = aDF[i].replace('?', value = str(ave), inplace=False)            
@@ -58,6 +62,53 @@ def replaceWithConditionalAverage(theDF):
     toReturn = pd.concat(frames)
     return toReturn
 
+def calcEuc(aDataframe):
+    dfC = aDataframe
+    dfC["ED"] = ""
+    r,c = aDataframe.shape
+    bigMat = [[0 for x in range(r)] for y in range(r)]
+    dfA = aDataframe
+    dfA = dfA.drop(columns='Class')
+    dfB = aDataframe
+    dfB = dfB.drop(columns='Class')
+    for x in range (0,r):
+        minimum = 1000
+        for y in range (x+1,r):
+            '''gross row calculations'''
+            z=0
+            theSum = 0
+            while (z < 8):
+                a = dfA.iat[x,z]
+                if(a == '?'):
+                    a = float(0)
+                a = float(a)
+                b = dfB.iat[y,z]
+                if(b == '?' or a == '?'):
+                    b = a
+                b = float(b)
+                theSum += ((a-b)*(a-b))
+                z += 1
+            theNext = ((math.sqrt(theSum))/2)
+            theNext = round(theNext,5)
+            if (theNext < minimum):
+                minimum = theNext
+                dfC.at[x, "ED"] = y
+            bigMat[x][y] = theNext
+            bigMat[y][x] = theNext
+            bigMat[x][x] = 0            
+            #print("We are at " + str(x) + " , " + str(y))
+            
+    print("Holy fucking shit it finished")
+    return
+
+''''''
+def hotDeck(theDF):
+    # https://stackoverflow.com/questions/15943769/how-do-i-get-the-row-count-of-a-pandas-dataframe
+    r,c = theDF.shape  #r = number of rows #c = numb of columns
+    p = 0
+    toReturnDF = theDF
+    return toReturnDF
+start_time = time.time()
 '''Read the files into python'''
 df = pd.read_csv('dataset_complete.csv')
 df05 = pd.read_csv('dataset_missing05.csv')
@@ -68,11 +119,12 @@ dfmock05 = pd.read_csv('dataset_missing05.csv')
 '''dataset with 5% missing computed with mean here''' 
 '''TODO: Calculate on the 20% missing data '''
 #allCol is new list object of the dfmock05 dataset
-allCol = list(dfmock05)
-##printHT(dfmock05) #Print for pre-test
+'''TODO : Why did I stop using this'''
+allCol = list(dfmock05) 
+printHT(dfmock05) #Print for pre-test
 #Parse each column, calculate the average in that column, replace all '?', iterate
 dfmock05 = replaceWithAverage(dfmock05) 
-##printHT(dfmock05) #Print for post-test
+printHT(dfmock05) #Print for post-test
 #use export to set this temporary dfmock05 dataset, adjusted, to filename below
 export = dfmock05.to_csv('V00396834_missing05_imputed_mean.csv', index= None, header=True)
 
@@ -83,17 +135,23 @@ print("\n------------Conditional Mean Imputation Start-------------\n")
 #Reset dfmock05 to the origial dataset from 'dataset_missing05.csv'
 dfmock05 = pd.read_csv('dataset_missing05.csv')
 allCol = list(dfmock05) #set allCol to list form of dfmock05
-##printHT(dfmock05) #Print for pre-test
+printHT(dfmock05) #Print for pre-test
 dfmock05 = replaceWithConditionalAverage(dfmock05)
-##printHT(dfmock05) #Print for post-test
+printHT(dfmock05) #Print for post-test
 export = dfmock05.to_csv('V00396834_missing05_imputed_mean_conditional.csv', index=None, header=True)
 
 
+print("\n------------Hot Deck Imputation-------------\n")
+'''Setting up for algorithm 3 Hot Deck Imputation on 5% missing'''
+'''TODO: Calculate on the 20% missing data '''
+#Reset dfmock05 to the origial dataset from 'dataset_missing05.csv'
+dfmock05 = pd.read_csv('dataset_missing05.csv')
+##printHT(dfmock05) #Print for pre-test
+EucDistance05 = calcEuc(dfmock05)
 
-
-
-
-
+dfmock05 = hotDeck(dfmock05)
+##printHT(dfmock05) #Print for post-test
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
